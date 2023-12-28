a19d6bf18b68422a29c34fe8c79afbeae897a7fc
YuriiBazhenov <bazhenovyura333@gmail.com>
2023-12-28 16:22:03 -0600

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,38 @@
+const { defineConfig } = require('cypress')
+const oracledb = require('oracledb')
+
+module.exports = defineConfig({
+  env: {
+    baseUrl: 'https://tech-global-training.com/students',
+    'oracleDb': {
+      'user': 'techglobaldev',
+      'password': '$techglobaldev123!',
+      'connectString': 'techglobal.cup7q3kvh5as.us-east-2.rds.amazonaws.com:1521/TGDEVQA'
+    }
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+      on('task', {
+        async runQuery(query) {
+          let connection
+          try {
+            // Establish connection to the Oracle Database
+            connection = await oracledb.getConnection(config.env.oracleDb)
+            const result = await connection.execute(query)
+            return result.rows
+
+          } catch (err) {
+            throw new Error(err)
+          } finally {
+            if (connection) {
+              // Ensure the connection is closed after execution
+              await connection.close()
+            }
+          }
+        }
+      })
+    },
+  },
+})
---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,139 @@
+
+import { postRequestBody, putRequestBody } from '../fixtures/user.json'
+
+describe('CRUD Operations', () => {
+
+    let studentID
+
+    beforeEach(function () {
+
+
+        cy.fixture('user').then((data) => {
+            this.firstName = data.postRequestBody.firstName
+            this.lastName = data.postRequestBody.lastName
+            this.email = data.postRequestBody.email
+            this.dob = data.postRequestBody.dob
+
+            this.firstName1 = data.putRequestBody.firstName
+            this.lastName1 = data.putRequestBody.lastName
+            this.email1 = data.putRequestBody.email
+            this.dob1 = data.putRequestBody.dob
+        })
+    })
+
+    it('Create a new student using POST', function () {
+
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('baseUrl'),
+            body: postRequestBody
+
+        }).then((response) => {
+            cy.log(JSON.stringify(response.body, null, 2))
+
+            studentID = response.body.id
+
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.below(5000)
+
+        })
+        cy.task('runQuery', 'SELECT * FROM student WHERE email = \'jasonS@gmail.com\'').then((rows) => {
+
+            const data = rows[0]
+
+            const expectedValues = [this.dob, this.email, this.firstName, this.lastName]
+
+            expectedValues.forEach((value, index) => {
+                expect(data[index + 1]).to.equal(value)
+            })
+
+        })
+    })
+
+
+    it('Retrieve a specific user-created', function () {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.env("baseUrl")}/${studentID}`,
+            body: postRequestBody
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.below(5000)
+        })
+
+        cy.task('runQuery', 'SELECT * FROM student WHERE email = \'jasonS@gmail.com\'').then((rows) => {
+
+            const data = rows[0]
+
+            const expectedValues = [this.dob, this.email, this.firstName, this.lastName]
+
+            expectedValues.forEach((value, index) => {
+                expect(data[index + 1]).to.equal(value)
+            })
+
+        })
+    })
+
+    it('Update an existing user', function () {
+        cy.request({
+            method: 'PUT',
+            url: `${Cypress.env("baseUrl")}/${studentID}`,
+            body: putRequestBody
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.below(5000)
+        })
+
+        cy.task('runQuery', 'SELECT * FROM student WHERE email = \'JackieC@gmail.net\'').then((rows) => {
+
+            const data = rows[0]
+
+            const expectedValues = [this.dob1, this.email1, this.firstName1, this.lastName1]
+
+            expectedValues.forEach((value, index) => {
+                expect(data[index + 1]).to.equal(value)
+            })
+
+        })
+    })
+
+    it('Retrieve a specific user created to confirm the update.', function () {
+        cy.request({
+            method: 'GET',
+            url: `${Cypress.env("baseUrl")}/${studentID}`,
+            body: putRequestBody
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.below(5000)
+        })
+
+        cy.task('runQuery', 'SELECT * FROM student WHERE email = \'JackieC@gmail.net\'').then((rows) => {
+
+            const data = rows[0]
+
+            const expectedValues = [this.dob1, this.email1, this.firstName1, this.lastName1]
+
+            expectedValues.forEach((value, index) => {
+                expect(data[index + 1]).to.equal(value)
+            })
+
+        })
+    })
+
+    it('Finally, delete the user that you created.', () => {
+
+        cy.request({
+            method: 'DELETE',
+            url: `${Cypress.env("baseUrl")}/${studentID}`
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.duration).to.be.below(5000)
+        })
+
+        cy.task('runQuery', 'SELECT * FROM student WHERE email = \'JackieC@gmail.net\'').then((rows) => {
+            expect(rows).to.have.length(0)
+        })
+    })
+})
+
+
